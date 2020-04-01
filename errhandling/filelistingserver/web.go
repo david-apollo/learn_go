e2f26a7cf72d9f992fe32bbf92ad9ffa13f879cb
342965391@qq.com <342965391@qq.com>
2020-04-01 10:46:50 +0800

    [modify] errWrapper

--- errhandling/filelistingserver/web.go
+++ errhandling/filelistingserver/web.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"os"
 	"learn_go/errhandling/filelistingserver/filelisting"
 	"net/http"
 )
@@ -9,15 +11,28 @@ import (
 type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
 
-func errWrapper(handler appHandler) func (writer http.ResponseWriter, request *http.Request)  {
-	
-} {
-	
+func errWrapper(handler appHandler) func(writer http.ResponseWriter, request *http.Request)  {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		err := handler(writer, request)
+		if err != nil{
+			log.Printf("Error handling request: %s", err.Error())
+			code := http.StatusOK
+			switch {
+			case os.IsNotExist(err):
+				code = http.StatusNotFound
+			case os.IsPermission(err):
+				code = http.StatusForbidden
+			default:
+				code = http.StatusInternalServerError
+			}
+			http.Error(writer, http.StatusText(code), code)
+		}
+	}
 }
 
 
 func main() {
-	http.HandleFunc("/list/", filelisting.HandleFileList)
+	http.HandleFunc("/list/", errWrapper(filelisting.HandleFileList))
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
