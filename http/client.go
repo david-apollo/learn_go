b8284b54d966683674e39ca5f982a6e809c1d35b
342965391@qq.com <342965391@qq.com>
2020-04-06 15:49:45 +0800

    [add] http

---
+++ http/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http/httputil"
+	"net/http"
+)
+
+func main() {
+	// resp, err := http.Get("http://www.imooc.com")
+	request, err := http.NewRequest(http.MethodGet, "http://www.imooc.com", nil)
+	request.Header.Add("User-Agent", 
+	"Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")
+	
+	client := http.Client{
+		CheckRedirect: func(
+			req *http.Request,
+			via []*http.Request) error {
+				fmt.Println("Redirect:", req)
+				return nil
+			},
+	}
+	// resp, err := http.DefaultClient.Do(request)
+	resp, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	s, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", s)
+}
